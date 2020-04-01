393e373beb3d50e857af69ee1b65bf7567395bc0
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-01 17:25:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken?(board,index)
-    true
-  elsif index.between?(0, 8)
-    true
-  else
     false
+  elsif index.between?(0, 8)
+    false
+  else
+    true
   end
 
 end
